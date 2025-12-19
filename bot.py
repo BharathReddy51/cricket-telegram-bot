0777499b17207b89406703e76cd710bf7c0eb1e7
Bharath Reddy <140046648+BharathReddy51@users.noreply.github.com>
2025-12-19 15:45:15 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -9,3 +9,11 @@ MATCH_ID = os.getenv("MATCH_ID")
 CHAT_ID = os.getenv("CHAT_ID")
 
 bot = Bot(token=BOT_TOKEN)
+async def main():
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text="🟢 Bot connected successfully!"
+    )
+
+    while True:
+        ...
