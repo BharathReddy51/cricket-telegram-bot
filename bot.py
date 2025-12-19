b587d99607592024e3ca29b9b5347fe3c604df6b
Bharath Reddy <140046648+BharathReddy51@users.noreply.github.com>
2025-12-19 15:42:30 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,5 +1,11 @@
-from telegram import Bot
 import os
+import asyncio
+import requests
+from telegram import Bot
 
-test_bot = Bot(token=os.getenv("8465888851:AAE6Za4sC5NM3CwA6k9mOmulbWpwsP2wrkg"))
-print(test_bot.get_me())
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_KEY = os.getenv("API_KEY")
+MATCH_ID = os.getenv("MATCH_ID")
+CHAT_ID = os.getenv("CHAT_ID")
+
+bot = Bot(token=BOT_TOKEN)
